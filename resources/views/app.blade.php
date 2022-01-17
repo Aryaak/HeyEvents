76cb1541ccf7f8c6bd49e02f51e99ae278d754c2
muhBilal <mb336450@gmail.com>
2022-01-17 19:37:18 +0700

    laravel

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1 class="text-red-50">test</h1>
+</body>
+</html>
