347902689c89375678f4fc43aa50edf813482530
muhBilal <mb336450@gmail.com>
2022-02-11 23:24:37 +0700

    modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,18 @@ require('./bootstrap');
 var Turbolinks = require("turbolinks")
 Turbolinks.start()
 
+// modal
+window.addEventListener('DOMContentLoaded', () =>{
+    const overlay = document.querySelector('#overlay')
+    const delBtn = document.querySelector('#btn')
+    const closeBtn = document.querySelector('#close-modal')
+
+    const toggleModal = () => {
+        overlay.classList.toggle('hidden')
+        overlay.classList.toggle('flex')
+    }
+
+    delBtn.addEventListener('click', toggleModal)
+
+    closeBtn.addEventListener('click', toggleModal)
+})
